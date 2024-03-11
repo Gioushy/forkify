dea3474929e04f5f7e60b52b43938eb6e782f4a8
Gioushy <mustafa.elgioshy@gmail.com>
2024-03-11 19:35:32 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_URL_POST = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ac62b9e8-96a0-4c25-b6a6-d0ca1983b0be';
+export const MODAL_CLOSE_SEC = 2.5;
